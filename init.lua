e000fa9a5b8f2a4b920c84ee11351f88dbd81c80
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2026-03-04 14:33:41 -0600

    backup

--- init.lua
+++ init.lua
@@ -1,5 +1,14 @@
 -- ia_persistencelib/init.lua
 
+assert(minetest.get_modpath('ia_util'))
+assert(ia_util ~= nil)
+local modname                    = minetest.get_current_modname() or "ia_persistencelib"
+local storage                    = minetest.get_mod_storage()
+persistencelib                   = {}
+local modpath, S                 = ia_util.loadmod(modname)
+local log                        = ia_util.get_logger(modname)
+local assert                     = ia_util.get_assert(modname)
+
 --assert(fakelib ~= nil)
 --
 --persistencelib = {}
@@ -120,7 +129,7 @@
 assert(fakelib ~= nil)
 assert(futil   ~= nil)
 
-persistencelib = {}
+--persistencelib = {}
 
 ---------------------------
 -- 1. Internal Helpers
